117ffc4a607e70d6c54a69bf11981ee0a9ca4da2
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-06 20:35:38 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+ {
+  "name": "dragon-xr",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot built by Raheem-cm",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "@adiwajshing/keyed-db": "^0.2.4",
+    "pino": "^8.19.0",
+    "qrcode-terminal": "^0.12.0",
+    "axios": "^1.6.0"
+  },
+  "author": "Raheem-cm"
+}
