63530057e52b279bbe50199e28cef511bd520ac9
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-06 20:33:20 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    botName: "DRAGON-XR",
+    ownerNumber: "255760003443",
+    ownerName: "Raheem-cm",
+    newsletterJid: "120363399470975987@newsletter",
+    channelLink: "https://whatsapp.com/channel/0029VbAffhD2ZjChG9DX922r",
+    repoLink: "https://github.com/Raheem-cm/must-go",
+    botImage: "https://files.catbox.moe/tdq4c0.jpeg",
+    prefix: "." // Alama ya kuanza amri, mfano .menu
+};
+
+module.exports = config;
