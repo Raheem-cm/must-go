33dc504a3fd5c747d41a18f3039d958683323e05
ABDULRAHIM-NYONI <nyoniraheem@gmail.com>
2026-01-06 21:06:40 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,17 +1,24 @@
- {
-  "name": "dragon-xr",
+{
+  "name": "dragon-xr-bot",
   "version": "1.0.0",
-  "description": "WhatsApp Bot built by Raheem-cm",
+  "description": "DRAGON-XR WhatsApp Bot",
   "main": "index.js",
   "scripts": {
-    "start": "node index.js"
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "node test.js"
   },
   "dependencies": {
-    "@whiskeysockets/baileys": "^6.6.0",
-    "@adiwajshing/keyed-db": "^0.2.4",
-    "pino": "^8.19.0",
+    "@whiskeysockets/baileys": "^6.5.0",
     "qrcode-terminal": "^0.12.0",
-    "axios": "^1.6.0"
+    "axios": "^1.6.0",
+    "fs-extra": "^11.0.0",
+    "chalk": "^4.1.2",
+    "moment-timezone": "^0.5.40",
+    "pino": "^8.0.0",
+    "dotenv": "^16.0.0"
   },
-  "author": "Raheem-cm"
+  "keywords": ["whatsapp-bot", "dragon-xr"],
+  "author": "DRAGON-XR Team",
+  "license": "MIT"
 }
